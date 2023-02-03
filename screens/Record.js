04f7a5603d8a00a57f2225bc86e6f053a09ea881
emmetbrowne <18238637@studentmail.ul.ie>
2023-02-03 12:58:23 +0000

    Fixing navigation issues , created routes folder

---
+++ screens/Record.js
@@ -0,0 +1,133 @@
+import { useNavigation } from "@react-navigation/core";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { auth } from "../firebase";
+
+// const Record = () => {
+// const navigation = useNavigation();
+// const [recording, setRecording] = React.useState();
+// const [recordings, setRecordings] = React.useState([]);
+// const [message, setMessage] = React.useState("");
+
+// const handleSignOut = () => {
+//   auth
+//     .signOut()
+//     .then(() => {
+//       navigation.replace("Login");
+//     })
+//     .catch((error) => alert(error.message));
+// };
+
+// async function startRecording() {
+//   try {
+//     const permission = await Audio.requestPermissionsAsync();
+
+//     if (permission.status === "granted") {
+//       await Audio.setAudioModeAsync({
+//         allowsRecordingIOS: true,
+//         playsInSilentModeIOS: true,
+//       });
+
+//       const { recording } = await Audio.Recording.createAsync(
+//         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+//       );
+
+//       setRecording(recording);
+//     } else {
+//       setMessage("Please grant permission to app to access microphone");
+//     }
+//   } catch (err) {
+//     console.error("Failed to start recording", err);
+//   }
+// }
+
+// async function stopRecording() {
+//   setRecording(undefined);
+//   await recording.stopAndUnloadAsync();
+
+//   let updatedRecordings = [...recordings];
+//   const { sound, status } = await recording.createNewLoadedSoundAsync();
+//   updatedRecordings.push({
+//     sound: sound,
+//     duration: getDurationFormatted(status.durationMillis),
+//     file: recording.getURI(),
+//   });
+
+//   setRecordings(updatedRecordings);
+// }
+
+// function getDurationFormatted(millis) {
+//   const minutes = millis / 1000 / 60;
+//   const minutesDisplay = Math.floor(minutes);
+//   const seconds = Math.round((minutes - minutesDisplay) * 60);
+//   const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+//   return `${minutesDisplay}:${secondsDisplay}`;
+// }
+
+// function getRecordingLines() {
+//   return recordings.map((recordingLine, index) => {
+//     return (
+//       <View key={index} style={styles.row}>
+//         <Text style={styles.fill}>
+//           Recording {index + 1} - {recordingLine.duration}
+//         </Text>
+//         <Button
+//           style={styles.button}
+//           onPress={() => recordingLine.sound.replayAsync()}
+//           title="Play"
+//         ></Button>
+//         <Button
+//           style={styles.button}
+//           onPress={() => Sharing.shareAsync(recordingLine.file)}
+//           title="Share"
+//         ></Button>
+//       </View>
+//     );
+//   });
+// }
+
+export default function Record() {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+      <Button
+        title={recording ? "Stop Recording" : "Start Recording"}
+        onPress={recording ? stopRecording : startRecording}
+      />
+      {getRecordingLines()}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "60%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 40,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fill: {
+    flex: 1,
+    margin: 16,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
